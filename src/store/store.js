f31e3ddecfceec391be007e029867f77939015fa
Angel <gutierrezramosjoseangel@gmail.com>
2021-07-20 14:43:16 -0500

    Config redux thunk to handle async requests

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,16 @@
-import { createStore, combineReducers } from 'redux'
-import { authReducer } from '../reducers/authReducer'
+import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { authReducer } from '../reducers/authReducer';
 
 const reducers = combineReducers({
     auth: authReducer
-})
+});
+
+const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
 
 export const store = createStore(
     reducers,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
 )
