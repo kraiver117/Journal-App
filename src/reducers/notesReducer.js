2b222f70ff5398ef0c55e7a29de97510a41544dd
Angel <gutierrezramosjoseangel@gmail.com>
2021-07-21 16:52:51 -0500

    Created note activated

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 const initialState = {
     note: [],
     active: null
@@ -5,9 +7,13 @@ const initialState = {
 
 export const notesReducer = (state = initialState, action) => {
     switch (action.type) {
-        case action:
-            
-            break;
+        case types.notesActive:
+            return {
+                ...state,
+                active: {
+                    ...action.payload
+                }
+            }
     
         default:
             return state;
