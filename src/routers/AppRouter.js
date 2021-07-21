b8e52c6a8e0341f1b25067ed010b5c7c49fec94b
Angel <gutierrezramosjoseangel@gmail.com>
2021-07-20 23:03:16 -0500

    Keep the user logged in redux state

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { firebase } from '../firebase/firebase-config';
+import React, { useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -7,9 +8,21 @@ import {
 } from "react-router-dom";
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName));
+            }
+        });
+    }, [dispatch]);
+
     return (
         <Router>
             <Switch>
