fd619fb5637ef170eeb7ca57225cbef16be787e3
Angel <gutierrezramosjoseangel@gmail.com>
2021-07-21 23:15:18 -0500

    Activate note for update

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import moment from 'moment';
+import { useDispatch } from 'react-redux';
+import { activeNote } from '../../actions/notes';
 
 export const JournalEntry = ({id, date, title, body, url}) => {
 
     const noteDate = moment(date);
+    const dispatch = useDispatch();
+
+    const handleEntryClick = () => {
+        dispatch(
+            activeNote(
+                id, {
+                title,
+                body,
+                date,
+                url
+            })
+        );
+    }
 
     return (
-        <div className="journal__entry pointer">
+        <div 
+            className="journal__entry pointer"
+            onClick={handleEntryClick}
+        >
             {
                 url &&
                 <div 
--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,19 +1,48 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useForm } from '../../hooks/useForm';
 import { NotesAppBar } from './NotesAppBar';
 
 export const NoteScreen = () => {
+
+    const {active: note} = useSelector(state => state.notes);
+    const [formValues, handleInputChange] = useForm(note);
+
+    const  { body, title } = formValues;
+
     return (
         <div className="notes__main-content">
 
             <NotesAppBar />
 
             <div className="notes__content">
-                <input type="text" placeholder="Some awesome title" className="notes__title-input" autoComplete="off" />
-                <textarea placeholder="What happened today?" className="notes__textarea"></textarea>
 
-                <div className="notes__image">
-                    <img src="https://miro.medium.com/max/700/1*dLaDL-lSN0iprzmOpmM7zQ.png" alt="ReactJS"/>
-                </div>
+                <input 
+                    type="text" 
+                    placeholder="Some awesome title" 
+                    className="notes__title-input" 
+                    autoComplete="off" 
+                    value={title}
+                    onChange={handleInputChange}
+                />
+
+                <textarea 
+                    placeholder="What happened today?" 
+                    className="notes__textarea"
+                    value={body}
+                    onChange={handleInputChange}
+                />
+
+                {
+                    (note.url) &&
+                    <div className="notes__image">
+                        <img 
+                            src="https://miro.medium.com/max/700/1*dLaDL-lSN0iprzmOpmM7zQ.png" 
+                            alt="ReactJS"
+                        />
+                    </div>
+                }
+                
             </div>
 
         </div>
