d850655f1bea757f52b83a536bb1ac79572768c7
Angel <gutierrezramosjoseangel@gmail.com>
2021-07-22 23:22:08 -0500

    Upload image to cloudinary

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,22 +1,46 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { startSaveNote } from '../../actions/notes';
+import { startSaveNote, startUploading } from '../../actions/notes';
 
 export const NotesAppBar = () => {
 
     const dispatch = useDispatch();
     const { active } = useSelector(state => state.notes);
 
+    const inputFile = useRef(null);
+
     const handleSave = () => {
         dispatch(startSaveNote(active));
+    }
+
+    const handlePictureUpload = () => {
+        inputFile.current.click();
+    }
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+
+        if (file) {
+            dispatch(startUploading(file));
+        }
     }
 
     return (
         <div className="notes__appbar">
             <span>28 de agosto 2020</span>
 
+            <input
+                ref={inputFile} 
+                type='file'
+                style={{display: 'none'}}
+                onChange={handleFileChange}
+            />
+
             <div>
-                <button className="btn">
+                <button 
+                    className="btn"
+                    onClick={handlePictureUpload}
+                >
                     Picture
                 </button>
 
